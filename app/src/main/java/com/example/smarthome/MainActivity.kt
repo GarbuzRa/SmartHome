1fb4a7cf0c74000b7051615d16542c9d7ebe594b
Corpseofpig <Corpseofpig@mail.ru>
2025-04-19 16:43:57 +0300

    TextField fix

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,15 +19,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
@@ -148,10 +158,95 @@ fun FlowerCard(flower: FlowerCardData) {
     }
 }
 
+@Composable
+fun MainScreen () {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Red)) {
+        Column(Modifier.fillMaxSize()) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .height(130.dp)
+                    .background(brush = Brush.verticalGradient(
+                        0.00f to Color(0xff010139),
+                        0.25f to Color(0xff1A1A73),
+                        0.50f to Color(0xff3333AD),
+                        1.00f to Color(0xff010139)
+                    ))
+                    .padding(16.dp)
+            ) {
+                // строка с иконками и текстом.
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.user),
+                        contentDescription = "User",
+                        tint = Color.White,
+                        modifier = Modifier.size(36.dp)
+                    )
+
+                    Text(
+                        text = "Follow flowers",
+                        color = Color.White,
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .weight(1f)
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                    )
+
+                    Icon(
+                        painter = painterResource(R.drawable.plus_group),
+                        contentDescription = "Group",
+                        tint = Color.White,
+                        modifier = Modifier.size(36.dp)
+                    )
+
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row( modifier = Modifier
+                    .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically) {
+                    var textFieldState by remember {
+                        mutableStateOf("")
+                    }
+
+                    TextField(
+                        modifier = Modifier
+                            .weight(1f)
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                            .background(brush = Brush.verticalGradient(
+                               0.50f to Color(0xff170057),
+                               1.00f to Color(0xff5F3BC3))
+                            ),
+                        value = textFieldState,
+                        onValueChange = {textFieldState = it},
+                    )
+                }
+            }
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun FlowerCardPreview() {
     val flower = FlowerCardData("Monstera Deliciosa", true, 63, 99, "")
     FlowerCard(flower)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    SmartHomeTheme { MainScreen() }
 }
